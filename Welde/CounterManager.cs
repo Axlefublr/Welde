78709079df67f06e0be65041dd6a332a60536607
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-05-14 19:52:40 +0800

    add CounterManager

---
+++ Welde/CounterManager.cs
@@ -0,0 +1,18 @@
+namespace Welde;
+
+public class CounterManager
+{
+
+	private readonly string counterFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "counter.txt");
+
+	private int Read()
+	{
+		string text = File.ReadAllText(counterFile);
+		bool success = int.TryParse(text, out int result);
+		if (!success)
+		{
+			throw new FormatException("Counter file isn't fully a integer");
+		}
+		return result;
+	}
+}

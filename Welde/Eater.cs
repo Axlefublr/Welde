8e72e8d8ade3840727b4765674b40645979a5f82
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-05-16 01:00:32 +0800

    implement methods to calculate date difference

--- Welde/Eater.cs
+++ Welde/Eater.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 namespace Welde;
 
 internal class Eater
@@ -15,5 +17,23 @@ internal class Eater
 
 	private string GetDateNowString() => DateTime.Now.ToString("yyyy.MM.dd");
 
+	private DateTime ParseDateFileIntoDateTime()
+	{
+		bool success = DateTime.TryParseExact(Read(), "yyyy.MM.dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime parsedDate);
+		if (!success)
+		{
+			throw new ArgumentException("Invalid date format. Use 'yyyy.MM.dd'");
+		}
+		return parsedDate;
 	}
+
+	private int GetDiffOfDays(DateTime prevDate)
+	{
+		TimeSpan difference = prevDate - DateTime.Now;
+		int daysDifference = Math.Abs((int)difference.TotalDays);
+		return daysDifference;
+	}
+
+	private string Read() => File.ReadAllText(dateFile);
+
 }
